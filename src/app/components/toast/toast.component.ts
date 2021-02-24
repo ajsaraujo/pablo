0e507a37128ec54a4da71db17c683f4d73e25f18
Allan Juan <allanjuansil@gmail.com>
2021-02-24 14:09:19 -0300

    Show toast

--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -1,24 +1,24 @@
 import { animate, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-toast',
   templateUrl: './toast.component.html',
   styleUrls: ['./toast.component.css'],
-  animations: [
-    trigger('inOut', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('1s ease-out', style({ opacity: 1 })),
-      ]),
-      transition(':leave', [
-        style({ opacity: 1 }),
-        animate('1s ease-in', style({ opacity: 0 })),
-      ]),
-    ]),
-  ],
 })
-export class ToastComponent {
-  constructor(public toastService: ToastService) {}
+export class ToastComponent implements OnDestroy {
+  message: String = '';
+  private subscription: Subscription;
+
+  constructor(public toastService: ToastService) {
+    this.subscription = this.toastService.message$.subscribe((message) => {
+      this.message = message;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/services/toast.service.ts
+++ src/app/services/toast.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ToastService {
-  isVisible = false;
-  message = '';
+  message$ = new BehaviorSubject<String>('');
+  private hideTimer: any;
 
   show(message: string) {
-    this.message = message;
-    this.isVisible = true;
+    this.message$.next(message);
 
-    setTimeout(() => this.hide(), 3000);
+    clearTimeout(this.hideTimer);
+    this.hideTimer = setTimeout(this.hide.bind(this), 2500);
   }
 
   private hide() {
-    this.message = '';
-    this.isVisible = false;
+    this.message$.next('');
   }
 }
