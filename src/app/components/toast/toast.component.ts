1d0787e9afb16c441dadfffadbe39b5a73fb8890
Allan Juan <allanjuansil@gmail.com>
2021-02-24 14:14:28 -0300

    Add toast fade animation

--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -7,6 +7,18 @@ import { ToastService } from 'src/app/services/toast.service';
   selector: 'app-toast',
   templateUrl: './toast.component.html',
   styleUrls: ['./toast.component.css'],
+  animations: [
+    trigger('inOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('1s ease-out', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('1s ease-in', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class ToastComponent implements OnDestroy {
   message: String = '';
