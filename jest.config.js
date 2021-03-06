34795579b1e188c41f3016e9c37f179242fc7565
Allan Juan <34658414+ajsaraujo@users.noreply.github.com>
2021-03-06 16:19:36 -0300

    Setup jest (#3)

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  preset: 'jest-preset-angular',
+  roots: ['<rootDir>/src/'],
+  testMatch: ['**/+(*.)+(spec).+(ts)'],
+  setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: '<rootDir>/'
+  })
+}
