adb3cbf9d839bff5058b7264605fe90f9be83bbc
Semla <semla.dev@gmail.com>
2021-10-13 22:43:21 +0200

    Fading in palette to new pos. when sidebar toggled

--- src/app/components/palette/palette.component.ts
+++ src/app/components/palette/palette.component.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
+import { animate, style, transition, trigger, state } from '@angular/animations';
 import { Palette } from 'src/app/models/palette';
 import { PaletteService } from 'src/app/services/palette.service';
+import { SidebarService } from 'src/app/services/sidebar.service';
 
 @Component({
   selector: 'app-palette',
   templateUrl: './palette.component.html',
   styleUrls: ['./palette.component.css'],
+  animations: [
+    trigger('fade-in', [
+      state('in', style({
+        opacity:1
+      })),
+      transition( 'longer => shorter',[
+        style({opacity:0}),
+        animate(800)
+      ] ),
+      transition( 'shorter => longer',[
+        style({opacity:0}),
+        animate(1300)
+      ] ),
+    ]),
+  ]
 })
 export class PaletteComponent {
   palette: Palette;
 
   private previousPaletteName = '';
 
-  constructor(public paletteService: PaletteService) {
+  constructor(
+    public paletteService: PaletteService,
+    public sidebarService: SidebarService
+  ) {
     this.palette = this.paletteService.activePalette$.value;
 
     this.paletteService.activePalette$.subscribe((palette) => {
